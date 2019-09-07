f467b5fa32e4f6306931c6842ffff9060c88c15f
Frank Zhao <frank@frankzhao.net>
2019-09-07 12:41:16 +1000

    Add google maps integration on /gmap

--- app/config.js
+++ app/config.js
@@ -1,1 +1,3 @@
-export const APIURL = 'http//:localhost:5000/'
+export const APIURL = 'http//:localhost:5000/';
+export const MAPS_API_KEY = process.env.MAPS_API_KEY;
+console.log(process.env.MAPS_API_KEY);
